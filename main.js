611c3fe306d312864b94accd7b4a5b630ba78275
ShravyaHub <shravyak710@gmail.com>
2020-12-10 15:50:06 +0530

    ADDED: code to validate email

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const NAME_REGEX = /^[A-Z][a-z]{2,}$/;
+const EMAIL_REGEX = /^([a-z0-9]+[-._+]?[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$/;
 
 var readlineSync = require('readline-sync');
 var utility = require('./utility');
@@ -8,3 +9,5 @@ var firstName = readlineSync.question("Enter first name: ");
 console.log(utility.validation(NAME_REGEX, firstName));
 var lastName = readlineSync.question("Enter last name: ");
 console.log(utility.validation(NAME_REGEX, lastName));
+var email = readlineSync.question("Enter email: ");
+console.log(utility.validation(EMAIL_REGEX, email));
