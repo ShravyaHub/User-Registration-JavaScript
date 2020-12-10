9257638d133df6bcced66a0f7a2cd83bd4148df7
ShravyaHub <shravyak710@gmail.com>
2020-12-10 16:02:10 +0530

    ADDED: code to validate phone number

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const NAME_REGEX = /^[A-Z][a-z]{2,}$/;
 const EMAIL_REGEX = /^([a-z0-9]+[-._+]?[a-z0-9]+)+@[a-z0-9-]+.[a-z]{2,3}.[a-z]{2,3}$/;
+const PHONE_NUMBER_REGEX = /^[9][1][ ][6-9][0-9]{9}$/;
 
 var readlineSync = require('readline-sync');
 var utility = require('./utility');
@@ -11,3 +12,5 @@ var lastName = readlineSync.question("Enter last name: ");
 console.log(utility.validation(NAME_REGEX, lastName));
 var email = readlineSync.question("Enter email: ");
 console.log(utility.validation(EMAIL_REGEX, email));
+var phoneNumber = readlineSync.question("Enter phone number: ");
+console.log(utility.validation(PHONE_NUMBER_REGEX, phoneNumber));
